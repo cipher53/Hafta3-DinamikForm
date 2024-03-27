196b850eec3a9bf6eb1f70bcf8418e715d65ddc3
cipher53 <153065459+cipher53@users.noreply.github.com>
2024-03-28 00:56:12 +0300

    Add files via upload

---
+++ DinamikForm/DinamikForm/Film.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinamikForm
+{
+    // FilmKategori adında bir enum tanımlanıyor.
+    public enum FilmKategori {
+        Korku, Aksiyon, BilimKurgu, Komedi
+    }
+    // Film sınıfı tanımlanıyor.
+    public class Film
+    {
+         // Film özellikleri tanımlanıyor.
+        public int FilmID { get; set; }
+        public string FilmAdı { get; set; }
+        public int FilmSüresi { get; set; }
+        public FilmKategori kategori { get; set; }
+
+         // Film sınıfının parametresiz yapıcısı (constructor).
+        public Film() { }
+
+         // Filmleri içeren bir List döndüren statik bir metot tanımlanıyor.
+        public static List<Film> FilmleriListele() 
+        {
+            // Örnek bir film listesi oluşturuluyor ve döndürülüyor.
+            return new List<Film>()
+            {
+                // Birkaç örnek film ekleniyor.
+                new Film(){
+                    FilmAdı = "Avatar",
+                    FilmSüresi = 180,
+                    kategori = FilmKategori.BilimKurgu,               
+                },
+                new Film(){
+                    FilmAdı = "Hayvan Mezarlığı",
+                    FilmSüresi = 150,
+                    kategori = FilmKategori.Korku,
+                },
+                new Film(){
+                    FilmAdı = "Intersteller",
+                    FilmSüresi = 180,
+                    kategori = FilmKategori.BilimKurgu,
+                },
+                new Film(){
+                    FilmAdı = "Recep İvedik",
+                    FilmSüresi = 200,
+                    kategori = FilmKategori.Komedi,
+                },
+                new Film(){
+                    FilmAdı = "Hızlı Öfkeli",
+                    FilmSüresi = 200,
+                    kategori = FilmKategori.Aksiyon,
+                },
+                new Film(){
+                    FilmAdı = "Kolpaçino",
+                    FilmSüresi = 120,
+                    kategori = FilmKategori.Komedi,
+                },
+            };
+        }
+
+
+    }
+}
---
+++ DinamikForm/DinamikForm/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikForm
+{
+    // DinamikForm namespace'inde Form1 adında bir sınıf tanımlanıyor.
+    public partial class Form1 : Form
+    {
+        // Film nesnelerini içeren bir List tanımlanıyor.
+        public List<Film> filmler;
+
+        // Form1 sınıfının kurucu metodu.
+        public Form1()
+        {
+             // Form bileşenleri başlatılıyor.
+            InitializeComponent();
+             // Film.FilmleriListele metodu çağrılarak film listesi oluşturuluyor.
+            filmler = Film.FilmleriListele();
+        }
+
+        // PictureBox nesnelerini ekrana yerleştiren metot.
+        public void PictureBoxListele()
+        {
+            // PictureBox'ların yerleştirileceği başlangıç koordinatları tanımlanıyor.
+            int x = 50;
+            int y = 50;
+            int xSonraki = 150;
+            int ySonraki = 150;
+
+            // PictureBox arka plan renkleri tanımlanıyor.
+            var renk = new Color[] { Color.DarkBlue, Color.DarkCyan, 
+                                    Color.DarkOliveGreen,Color.DarkOrange,
+                                    Color.DarkMagenta,Color.DarkOrchid};
+
+            // Belirli sayıda PictureBox oluşturuluyor.
+            for (int i = 0; i < 5; i++)
+            {
+                // Belirli sayıda PictureBox oluşturuluyor.
+                PictureBox pb = new PictureBox();
+                // PictureBox'ın konumu belirleniyor.
+                pb.Location = new Point(x, y);
+                // PictureBox'ın arka plan rengi atanıyor.
+                pb.BackColor = renk[i];
+                // PictureBox'ın ismi FilmAdı ve kategoriye göre oluşturuluyor.
+                pb.Name = filmler[i].FilmAdı + filmler[i].kategori;
+                // PictureBox'a tıklandığında gerçekleşecek olay atanıyor.
+                pb.Click += new EventHandler(pb_Click);
+
+                // PictureBox formun kontrol listesine ekleniyor.
+                this.Controls.Add(pb);
+
+                // PictureBox'ların yerleştirilme koordinatları güncelleniyor.
+                if (x < 250)
+                {
+                    x = x + xSonraki;
+                }
+                else {
+                    x = 50;
+                    y = y + ySonraki;
+                }
+            }
+        }
+        // PictureBox'a tıklandığında gerçekleşecek olay.
+        private void pb_Click(object sender, EventArgs e)
+        {
+            // PictureBox nesnesi alınıyor.
+            PictureBox picture = sender as PictureBox;
+            // PictureBox'ın ismi MessageBox ile gösteriliyor.
+            MessageBox.Show(picture.Name);
+            
+        }
+
+        // Form yüklendiğinde gerçekleşecek olay.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // PictureBox'lar form yüklendiğinde listeleniyor.
+            PictureBoxListele();
+        }
+
+    }
+}
